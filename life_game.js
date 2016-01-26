5cb5241e7e98a6a1e28a64198ce3b643b98eac0e
psa-dfegnoux <denis.fegnoux@ext.mpsa.com>
2016-01-26 17:39:26 +0100

    [notFinished] newTurn function + add previous + current table array

--- life_game.js
+++ life_game.js
@@ -13,10 +13,10 @@ lifeGame = {
 
 		var btnStart = document.getElementById('btn-start');
 		var btnStop = document.getElementById('btn-stop');
-		var speed = document.getElementById('input-speed');
-		if(speed.value && typeof speed.value === "number"){
-			this.options.speed = speed.value;
-		}
+		// var speed = document.getElementById('input-speed');
+		// if(speed.value && typeof speed.value === "number"){
+		// 	this.options.speed = speed.value;
+		// }
 		btnStart.addEventListener('click', function(){
 			this.stopInterval = false;
 			this.lifeStart(this.options.speed);
@@ -34,19 +34,25 @@ lifeGame = {
 			var line = document.createElement('tr');
 			self.currentTable[x] = [];
 			for(var y = 1; y <= tableSize; y++){
-				self.currentTable[x][y] = "";
 				var cell = document.createElement('td');
 				cell.dataset.x = x;
 				cell.dataset.y = y;
 				cell.addEventListener('click', function() {
 					self.toggleCellStatus(this);
 				});
+				self.currentTable[x][y] = cell;
 				line.appendChild(cell);
 			}
 			table.appendChild(line);
 		}
+		self.renderTable();
 		console.log(self.currentTable);
 		document.getElementById('life-container').appendChild(table);
+	},
+	renderTable: function() {
+		if(this.currentTable.length){
+
+		}
 	},
 	toggleCellStatus: function(cell) {
 		console.log(cell.dataset.x+'-'+cell.dataset.y);
@@ -66,6 +72,25 @@ lifeGame = {
 	},
 	nextTurn: function() {
 		console.log('### NEW TURN ###');
+		this.previousTable = this.currentTable;
+		for(var x = 1; x <= this.options.tableSize; x++){
+			for(var y = 1; y <= this.options.tableSize; y++){
+				this.checkNeighbour(x,y);
+				this.toggleCellStatus(this.currentTable[x][y]);
+			}
+		}
+	},
+	checkNeighbour: function(x, y) {
+		var nbNeighbour = 0;
+		for(var i=-1; i<= 1; i++){
+			for(var y=-1; y<= 1; y++){
+				if((x+i) > 0 && (x+i) < this.tableSize && (x+i !== 0 && j !== 0)){
+					if(this.previousTable[x+i][y+j].className === "alive") {
+						nbNeighbour++;
+					}
+				}
+			}
+		}
 	}
 
 }
